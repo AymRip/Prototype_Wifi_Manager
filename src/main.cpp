c65e00de1e0b74974f8c2a39074ab75343f4c264
Aymeric Landry <1832372@cegeprdl.ca>
2020-10-09 12:05:49 -0400

    Prototype final Wifi_Manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <WiFiManager.h>
+
+WiFiManager wm;
+const char* ssid = "Aquarium";
+const char* password = "aquarium";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+    delay(1000);
+    Serial.println("\n");
+
+    if(!wm.autoConnect(ssid, password))
+        Serial.println("Erreur de connexion.");
+    else
+        Serial.println("Connexion etablie!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /*if(touchRead(T0) < 50)
+  {
+    Serial.println("Suppression des reglages et redemarrage...");
+    wm.resetSettings();
+    ESP.restart();
+  }*/
 }
